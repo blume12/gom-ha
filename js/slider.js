24469447d19537d6b323152033db3f835836a445
Jasmin <minestern@web.de>
2016-06-30 10:37:20 +0200

    Add Slider to the start page

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $('.my-slider').unslider( {
+        "nav":false,
+        arrows: {
+            //  Unslider default behaviour
+            prev: '<a class="unslider-arrow prev"></a>',
+            next: '<a class="unslider-arrow next"></a>',
+        }
+    });
+});
